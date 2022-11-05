f785d19b2f9a1c351c6b8b04851600bb36643aa6
HoroTW <horo@horotw.com>
2022-11-05 19:30:18 +0100

    Add support for older Pillow versions resolves #1

--- razer_battery_tray.py
+++ razer_battery_tray.py
@@ -8,12 +8,24 @@
 import os
 import sys
 from time import sleep
+import pystray
 from pystray import Icon, Menu, MenuItem
+import openrazer
 import openrazer.client
 import PIL.Image
 
 RESIZE_TO_PIXELS = 32
 UPDATE_INTERVAL_IN_SECS = 2
+
+print("Starting Razer Battery Tray")
+
+try:
+    print("Pillow version: ", PIL.__version__)
+    print("Pystary version: ", pystray.__version__)
+    print("Openrazer version: ", openrazer.__version__)
+    print("Python version: ", sys.version)
+except:
+    print("Failed to get version info, please check your installation")
 
 
 a = openrazer.client.DeviceManager()
@@ -51,10 +63,15 @@ def get_icon(bat_level, charging=False):
         icon_name = f"icons/bat_{bat_level}_c.png"
 
     pil_icon = PIL.Image.open(icon_name)
-
     scale = (RESIZE_TO_PIXELS, RESIZE_TO_PIXELS)
-    pil_icon = pil_icon.resize(scale, PIL.Image.Resampling.LANCZOS)
+    downscale_method = None
 
+    if hasattr(PIL.Image, "Resampling"):  # Pillow >= 9.0
+        downscale_method = PIL.Image.Resampling.LANCZOS
+    else:  # Pillow < 9.0
+        downscale_method = PIL.Image.LANCZOS
+
+    pil_icon = pil_icon.resize(scale, downscale_method)
     return pil_icon
 
 
