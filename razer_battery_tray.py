e54454e9d5da8d9a58ad75f2e10a7de22c32c6c5
HoroTW <horo@horotw.com>
2022-11-06 01:31:26 +0100

    Allow for dynamic working path

--- razer_battery_tray.py
+++ razer_battery_tray.py
@@ -14,11 +14,15 @@ UPDATE_INTERVAL_IN_SECS = 2
 
 print("Starting Razer Battery Tray")
 
+script_path = os.path.dirname(__file__)
+icons_path = os.path.join(script_path, "icons")
+
 try:
     print("Pillow version: ", PIL.__version__)
     print("Pystary version: ", pystray.__version__)
     print("Openrazer version: ", openrazer.__version__)
     print("Python version: ", sys.version)
+    print("Path of the icons:", icons_path)
 except:
     print("Failed to get version info, please check your installation")
 
@@ -52,22 +56,10 @@ def get_icon(bat_level, charging=False):
     """
     Get the icon for the given battery level
     """
-
-    icon_name = f"icons/bat_{bat_level}.png"
-    if charging:
-        icon_name = f"icons/bat_{bat_level}_c.png"
-
-    pil_icon = PIL.Image.open(icon_name)
-    scale = (RESIZE_TO_PIXELS, RESIZE_TO_PIXELS)
-    downscale_method = None
-
-    if hasattr(PIL.Image, "Resampling"):  # Pillow >= 9.0
-        downscale_method = PIL.Image.Resampling.LANCZOS
-    else:  # Pillow < 9.0
-        downscale_method = PIL.Image.LANCZOS
-
-    pil_icon = pil_icon.resize(scale, downscale_method)
-    return pil_icon
+    name = f"bat_{bat_level}.png" if not charging else f"bat_{bat_level}_c.png"
+    icon_path = os.path.join(icons_path, name)
+    icon = PIL.Image.open(icon_path).resize(SCALE, DOWNSCALE_METHOD)
+    return icon
 
 
 def setup_icon(icon):
